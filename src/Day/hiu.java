510564ed32b433a60a9d775f35ab729b8c255434
LeeHyeon2 <sd0315@daum.net>
2022-04-21 11:34:47 +0900

    수정

--- src/Day/hiu.java
+++ src/Day/hiu.java
@@ -1,10 +1,10 @@
 package Day;
 
-public class Day_0421 {
+public class hiu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+
 	}
 
 }
